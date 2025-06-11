666f92ebffdb9a1cff5010f6fd5b2406f83868a5
Welexon <164246661+Welexon@users.noreply.github.com>
2025-06-11 11:35:17 +0200

    Add eslint tsconfig and update config

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default tseslint.config(
       sourceType: 'module',
       parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
       },
     },
     plugins: {
