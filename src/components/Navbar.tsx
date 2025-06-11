79799710d6aae635872c09e516d54be12f95653e
Welexon <164246661+Welexon@users.noreply.github.com>
2025-06-11 10:21:16 +0200

    chore: rename ryplix styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -226,7 +226,7 @@ const Navbar = () => {
           </Link>
           <Link
             to="/register"
-            className="ryplix-button"
+            className="codenautica-button"
           >
             Sign up
           </Link>
@@ -401,7 +401,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="ryplix-nav sticky top-0 z-50">
+    <nav className="codenautica-nav sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
